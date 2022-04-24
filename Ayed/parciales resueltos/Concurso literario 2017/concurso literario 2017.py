3453f06ff47fa6d923bd7ce45071716bbba1e99f
Gaston Mendoza <72703358+playthafunkmusic@users.noreply.github.com>
2022-04-24 10:52:34 -0300

    Update concurso literario 2017.py

--- Ayed/parciales resueltos/Concurso literario 2017/concurso literario 2017.py
+++ Ayed/parciales resueltos/Concurso literario 2017/concurso literario 2017.py
@@ -4,7 +4,7 @@ totalF = 0
 totalC = 0
 totalP = 0
 
-for escuela in range(10):
+for escuela in range(11):
 	cantE = 0
 	print('nro ',escuela)
 	dni= input('ingresar dni: ')
@@ -32,7 +32,7 @@ print('2 - Escuela que presento mas obras. ')
 print('3 - Cantidad de obras de cada genero. ')
 print('4 - Salir. ')
 opcion = input('ingresar opcion: ')
-while opcion >= '1' and opcion <= '4':
+while opcion < '1' and opcion > '4':
 	opcion = input('ingresar opcion: ')
 	os.system('cls')
 
@@ -42,11 +42,14 @@ while opcion != '4':
 	elif opcion == '2':
 		print(f'La escuela {mayorEsc} presento la mayor cantidad de obras.')
 	elif opcion == '3':
-		print(f'Fabula: {totalF} Cuento: {totalC} Poesia: {totalP}')
+		print(f'Fabula: {totalF} \nCuento: {totalC} \nPoesia: {totalP}')
 	input()	
 	os.system('cls')	
-	print('1 - cantidad de obras. ')
+	print('1 - Cantidad de obras. ')
 	print('2 - Escuela que presento mas obras. ')
-	print('3 - cantidad de obras de cada genero. ')
+	print('3 - Cantidad de obras de cada genero. ')
 	print('4 - salir. ') 	
-	opcion = input('ingresar opcion: ')	
+	opcion = input('ingresar opcion:')
+	while opcion < '1' and opcion > '4':
+		opcion = input('ingresar opcion:')
+	
