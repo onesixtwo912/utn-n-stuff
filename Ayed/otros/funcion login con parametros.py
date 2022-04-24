449eae27ef93f35d59924b4b876f0f09698e87e5
Gaston Mendoza <72703358+playthafunkmusic@users.noreply.github.com>
2022-04-24 07:50:42 -0300

    Create funcion login con parametros.py

---
+++ Ayed/otros/funcion login con parametros.py
@@ -0,0 +1,13 @@
+def login(password,acceso):
+    global pw, intentos
+    os.system('cls')
+    pw = input('ingresar password: ')
+    intentos = 0
+    while intentos < 2 and pw != password:
+        intentos = intentos + 1
+        print('password incorrecto. Intente nuevamente')
+        pw = input('ingresar password: ')
+    if pw == password:
+        return acceso()
+    else:
+        print('Ha superado el limite de ingresos erroneos.')
