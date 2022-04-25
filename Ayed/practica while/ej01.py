49706e0aa93fc4a8b208ea06bc8bdadc4c3fd41d
Gastón <72703358+playthafunkmusic@users.noreply.github.com>
2022-04-24 23:58:57 -0300

    Update ej01.py

--- Ayed/practica while/ej01.py
+++ Ayed/practica while/ej01.py
@@ -4,5 +4,4 @@ while nro != 0:
 	if nro >= 100:
 		cont = cont + 1
 	nro = int(input('ingresar un numero: '))	
-print (f'Hay {cont} numeros mayores o iguales a 100')		
-
+print (f'Hay {cont} numeros mayores o iguales a 100')
