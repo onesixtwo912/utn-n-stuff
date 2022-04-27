ae88ab907481584e4296a5f4063d326bf4876d72
Gastón <72703358+playthafunkmusic@users.noreply.github.com>
2022-04-27 06:13:50 -0300

    Update ej12.py

--- Ayed/practica while/ej12.py
+++ Ayed/practica while/ej12.py
@@ -1,16 +1,17 @@
-mayorCat = 0
+max = 0
 cat = int(input('ingresar categoria: '))
 
 while (cat >= 1) and (cat <= 20):
 	aux = cat 
-	mayorKm = 0
+	totalKm = 0
 	while cat == aux:
 		auto = input('ingresar auto: ')
 		km = float('ingresar kilometraje: ') 
 		monto = float('ingresar monto: ')
-		if monto > mayorCat:
-			mayorCat = cat 
-		mayorKm = mayorKm + km 
+		totalKm = totalKm + km 
+		if monto > max:
+			mayorCat = cat
+			max = monto
 		cat = int(input('ingresar categoria: '))
-	print(f'La categoria {aux} recorrio: {mayorKm}')	
+	print(f'La categoria {aux} recorrio: {totalKm}')	
 print(f'La categoria {mayorCat} realizo el viaje con mayor monto')
