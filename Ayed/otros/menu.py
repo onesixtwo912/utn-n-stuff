826b2ba2be5015f49e9c9cd83a1e6f15e579ed41
Gastón <72703358+playthafunkmusic@users.noreply.github.com>
2022-04-29 11:17:28 -0300

    Update menu.py

--- Ayed/otros/menu.py
+++ Ayed/otros/menu.py
@@ -2,12 +2,10 @@ import os
 def menu():    
     os.system('cls')
     mostrarmenu = '1 - xxx \n2 - yyy \n3 - zzz \n4 - Salir'
-    print(mostrarmenu)
-    opcion = input('Ingresar opcion: ')
+    opcion = input(f'{mostrarmenu} \nIngresar opcion: ')
     while opcion < '1' or opcion > '4':
         os.system('cls')
-        print(mostrarmenu)
-        opcion = input('Ingresar opcion: ')
+        opcion = input(f'{mostrarmenu} \nIngresar opcion: ')
     while opcion != '4':
         if opcion == '1':
             print(f'xxx')
@@ -17,9 +15,7 @@ def menu():
             print(f'zzz')
         input()
         os.system('cls')
-        print(mostrarmenu)
-        opcion = input('Ingresar opcion: ')
+        opcion = input(f'{mostrarmenu} \nIngresar opcion: ')
         while opcion < '1' or opcion > '4':
             os.system('cls')
-            print(mostrarmenu)
-            opcion = input('Ingresar opcion: ')
+            opcion = input(f'{mostrarmenu} \nIngresar opcion: ')
