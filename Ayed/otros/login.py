75c8c305ea1f7c26c6f5559013c4e3a06f4a424f
Gastón <72703358+playthafunkmusic@users.noreply.github.com>
2022-05-04 14:56:31 -0300

    Update login.py

--- Ayed/otros/login.py
+++ Ayed/otros/login.py
@@ -1,13 +1,11 @@
 import os
 def login(password,acceso):
-    global pw, intentos
-    os.system('cls')
+    global pw, intentos;os.system('cls')
     pw = input('ingresar password: ')
     intentos = 0
     while intentos < 2 and pw != password:
         intentos = intentos + 1
-        print('password incorrecto. Intente nuevamente')
-        pw = input('ingresar password: ')
+        pw = input('password incorrecto. Intente nuevamente: ')
     if pw == password:
         acceso()
     else:
