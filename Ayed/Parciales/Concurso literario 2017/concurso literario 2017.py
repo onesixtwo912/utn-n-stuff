b35e9ca5648aa438f03ade712733625645075af7
Gastón <72703358+playthafunkmusic@users.noreply.github.com>
2022-05-10 05:45:34 -0300

    Update concurso literario 2017.py

--- Ayed/Parciales/Concurso literario 2017/concurso literario 2017.py
+++ Ayed/Parciales/Concurso literario 2017/concurso literario 2017.py
@@ -2,10 +2,9 @@ import os
 def menu():    
 	os.system('cls')
 	mostrarmenu = '1 - Cantidad de obras. \n2 - Escuela que presento mas obras. \n3 - Cantidad de obras de cada genero. \n4 - Salir'
-	opcion = input(f'{mostrarmenu} \nIngresar opcion: ')	
+	opcion = input(f'{mostrarmenu} \nIngresar opcion: '); os.system('cls')	
 	while opcion < '1' or opcion > '4':
-		os.system('cls')
-		opcion = input(f'{mostrarmenu} \nIngresar opcion: ')	
+		opcion = input(f'{mostrarmenu} \nIngresar opcion: '); os.system('cls')
 	while opcion != '4':	
 		if opcion == '1':
 			print(f'Total de obras presentadas: {totalF + totalC + totalP}')
@@ -13,8 +12,7 @@ def menu():
 			print(f'La escuela {mayorEsc} presento la mayor cantidad de obras.')
 		elif opcion == '3':
 			print(f'Fabula: {totalF} \nCuento: {totalC} \nPoesia: {totalP}')
-		input()	
-		os.system('cls')	
+		input(); os.system('cls')	
 		opcion = input(f'{mostrarmenu} \nIngresar opcion: ')	
 		while opcion < '1' or opcion > '4':
 			opcion = input(f'{mostrarmenu} \nIngresar opcion: ')	
@@ -22,8 +20,7 @@ def menu():
 mayor = totalF = totalC = totalP = 0
 for escuela in range(1,11):
 	cantE = 0
-	print(f'Escuela N° {escuela}')
-	dni= input('ingresar dni: ')
+	dni= input(f'Escuela N° {escuela} \nIngresar dni: ')
 	while dni != '0':
 		genero = input('ingresar genero: ').upper()
 		while (genero != 'F') and (genero != 'C') and (genero != 'P'):
@@ -34,9 +31,8 @@ for escuela in range(1,11):
 			totalC = totalC + 1
 		elif genero == 'P':
 			totalP = totalP + 1
-		cantE = cantE + 1	
+		cantE = cantE + 1		
 		dni= input('ingresar dni: ')	
 	if cantE > mayor:
-		cantE = mayor
-		mayorEsc = escuela
+		cantE, mayorEsc = mayor, escuela
 menu()
