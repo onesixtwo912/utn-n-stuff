c22c580dd8023e46b1c38452e644268c5b60889b
Gastón <72703358+playthafunkmusic@users.noreply.github.com>
2022-05-17 16:39:59 -0300

    Update Censo 2022.py

--- Ayed/Parciales/Censo 2022/Censo 2022.py
+++ Ayed/Parciales/Censo 2022/Censo 2022.py
@@ -1,17 +1,15 @@
 import os
 def menu():
     mostrarmenu = '1 - Total de personas encuestadas. \n2 - Código de censo de la vivienda con mayor cantidad de personas \n3 - Porcentaje de viviendas habitadas respecto del total de viviendas asignadas. \n4 - Salir'
-    opcion = input(f'{mostrarmenu} \nIngresar opcion: ');os.system('cls')
-    while opcion < '1' or opcion > '4':
-        opcion = input(f'{mostrarmenu} \nIngresar opcion valida: ');os.system('cls')
+    opcion = ''
     while opcion != '4':
         if opcion == '1':
-            print(acum_h)
+            print(acum_h); input()
         elif opcion == '2':
-            print(max_codigo)
+            print(max_codigo); input()
         elif opcion == '3':
-            print(tot_h/c_viviendas*100)
-        input() ; os.system('cls')
+            print(tot_h/c_viviendas*100); input()
+        os.system('cls')
         opcion = input(f'{mostrarmenu} \nIngresar opcion: ');os.system('cls')
         while opcion < '1' or opcion > '4':
             opcion = input(f'{mostrarmenu} \nIngresar opcion valida: ');os.system('cls')
@@ -20,11 +18,11 @@ def proceso():
     global k, acum_h,max_codigo,tot_h,c_viviendas
     acum_h = max = 0
     c_viviendas = int(input('Ingresar cantidad de viviendas: '))
-    for k in range(1,c_viviendas):
+    for k in range(1,c_viviendas+1):
         codigo = int(input('Ingresar codigo de vivienda: '))
         while codigo < 1 or codigo > 999:
             codigo = int(input('Ingresar codigo de vivienda: '))
-        vh = input('¿Vivienda habitada? S/N').upper()
+        vh = input('¿Vivienda habitada? S/N: ').upper()
         while vh not in 'SN':
             vh = input('¿Vivienda habitada? S/N: ').upper()
         if vh == 'S':
@@ -33,7 +31,7 @@ def proceso():
             tot_h += 1
             if acum_h > max:
                 max = acum_h
-                max_codigo = tot_h
+                max_codigo = codigo
 
 tot_h = 0
 censista = input('Ingresar codigo de censista: ')
@@ -41,4 +39,4 @@ while censista != '0':
     proceso ()
     menu()
     censista = input('Ingresar codigo de censista: ')
-print (f'Total general de viviendas habitadas:{tot_h} ') 
+print (f'Total general de viviendas habitadas:{tot_h} ')
