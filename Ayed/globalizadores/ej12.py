9a348f763c5b9187e7b168f14676c749e1a8b431
Gastón <72703358+playthafunkmusic@users.noreply.github.com>
2022-05-17 03:42:22 -0300

    Update ej12.py

--- Ayed/globalizadores/ej12.py
+++ Ayed/globalizadores/ej12.py
@@ -1,7 +1,9 @@
 texto = input('Ingresar texto: ').lower()
 letra = input('Ingresar letra: ').lower()
-cont = 1 if texto[(len(texto)-1)] == letra else cont := 0 
-
+cont = 0 
 for pos in range(len(texto)-1):
     cont += 1 if texto [pos] == letra and texto [pos+1] in ',. )"' else 0
-print (f'{cont} palabras terminan con la letra {letra}. ')
+if texto[(len(texto)-1)] == letra: 
+    print (f'{cont+1} palabras terminan con la letra {letra}. ') 
+else: 
+    print (f'{cont} palabras terminan con la letra {letra}. ')
