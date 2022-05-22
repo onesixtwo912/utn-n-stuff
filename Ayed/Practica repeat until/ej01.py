4a61cf16b329729a241b8c2239daab886f5245ec
Gastón <72703358+playthafunkmusic@users.noreply.github.com>
2022-05-22 20:54:53 -0300

    Update ej01.py

--- Ayed/Practica repeat until/ej01.py
+++ Ayed/Practica repeat until/ej01.py
@@ -1,6 +1,6 @@
 cont = 0
 c = input('ingresar caracter: ')
-while (c != 'S') and (c != 's'):
-	cont = cont + 1
+while c != 'S':
+	cont += 1
 	c = input('ingresar caracter: ')
-print('{cont} caracteres')	
+print(f'{cont} caracteres')
