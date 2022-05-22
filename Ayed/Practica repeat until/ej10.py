2bbdd70e9f6519f096454c7ecbd5d7a9cf9a0f90
Gastón <72703358+playthafunkmusic@users.noreply.github.com>
2022-05-22 20:54:35 -0300

    Update ej10.py

--- Ayed/Practica repeat until/ej10.py
+++ Ayed/Practica repeat until/ej10.py
@@ -1,21 +1,21 @@
 cheques = max = 0 
 saldo = float(input('Ingresar saldo: '))
-def leer_cod():
-	global codigo
+codigo = input('Ingresar codigo: ').upper()
+while codigo not in 'CVF':
 	codigo = input('Ingresar codigo: ').upper()
-	while codigo not in 'CVF':
-		codigo = input('Ingresar codigo: ').upper()
-leer_cod()
 while codigo != 'F':
 	importe = float(input('Ingresar importe: '))
 	if codigo == 'C':
 		if saldo >= importe:
-			saldo = saldo - importe
+			saldo -= importe
 		else:
-			print(f'Saldo insuficiente. Valor faltante:{importe -saldo}')
-			cheques = cheques + 1
+			print(f'Saldo insuficiente. Dinero faltante: ${importe - saldo}')
+			cheques += 1
+			saldo = 0
 	else:
-		saldo = saldo + importe 
+		saldo += importe
 		max = importe if importe > max else 0
-	leer_cod()		
-print(f' Saldo: {saldo} \n Cheques emitidos: {cheques} \n Valor de venta maxima: {max}')
+	codigo = input('Ingresar codigo: ').upper()
+	while codigo not in 'CVF':
+		codigo = input('Ingresar codigo: ').upper()
+print(f' Saldo: ${saldo} \n Cheques emitidos: {cheques} \n Valor de venta maxima: ${max}')
