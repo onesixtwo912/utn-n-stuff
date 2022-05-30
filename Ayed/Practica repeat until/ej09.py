0d50b635b57f4910ad329abf180d5b221fb018a2
Gastón <72703358+playthafunkmusic@users.noreply.github.com>
2022-05-30 20:59:54 -0300

    Update ej09.py

--- Ayed/Practica repeat until/ej09.py
+++ Ayed/Practica repeat until/ej09.py
@@ -1,15 +1,13 @@
-import math
 n1 = int(input('ingresar nro: '))
 n2 = int(input('ingresar nro: '))
-#n1 = 40
-#n2 = 10
 n3 = 0
+print('columna 1     columna 2')
 while n1 != 0:
 	if n1 % 2 == 0:
-		print(f'{n1}         {n2}')
+		print(f'   {n1}            {n2}')
 	else:
 		n3 = n3 + n2
-		print(f'{n1}x       {n2}  {n3}')
-	n1 = math.floor(n1/2)
+		print(f'   {n1} x         {n2}     {n3}')
+	n1 = n1//2
 	n2 = n2 * 2		
-print(f'{math.floor(n1/2)}')#,'        ', n2)		
+print(f'   {n1//2}')
