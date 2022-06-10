f2039c93b8eb62cec11d9d6ff9a68d69ab94685b
Gastón <72703358+playthafunkmusic@users.noreply.github.com>
2022-06-10 10:43:01 -0300

    Update ej19.py

--- Ayed/Practica Arrays/Unidimensionales/ej19.py
+++ Ayed/Practica Arrays/Unidimensionales/ej19.py
@@ -1,31 +1,31 @@
 def inicio(x):
-    for i in range(12):
+    for i in range(n):
             x[i] = (chr(i+65))
 
 def carga(x):
-    codigo = validar('A','L','*')
+    codigo = validar('A',chr(n+65),'*') 
     while codigo != '*':
         horas = float(input('horas: '))
         x [ord(codigo)-65] += horas
-        codigo = validar('A','L','*')    
+        codigo = validar('A',chr(n+65),'*')    
 
-def validar(min,max,y):
+def validar(min,max,fin):
     k = input('codigo: ').upper()
-    while not (k >= min and k <= max or k == y):
+    while (k < min or k > max) and k != fin:
         k = input('codigo: ').upper()
     return k
 
 def orden(x,y):
-    for i in range(len(y)-1):
-        for k in range(i+1,len(y)):
+    for i in range(n-1):
+        for k in range(i+1,n):
             if x[i] < x[k]:
                 aux = x[i]; x[i] = x[k]; x[k] = aux
                 aux2 = y[i]; y[i] = y[k]; y[k] = aux2
 
 def muestra (x,y):
-    print(f'\tCODIGO DE AVION\t   TIEMPO DE VUELO\n')
+    print(f'\tCODIGO DE AVION\t   TIEMPO DE VUELO TOTAL\n')
     for i in range(len(b)):
-        print(f'\t\t{y[i]}\t\t{x[i]}')
+        print(f'\t\t{y[i]}\t\t  {x[i]}')
 
-a,b = 12*[0], 12*['']
+n = 12; a,b = n*[0], n*['']
 inicio(b), carga(a), orden(a,b), muestra(a,b)
