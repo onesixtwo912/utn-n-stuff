879999d0546dca1efd24c4c1bdb4e832258edd01
Gastón <72703358+playthafunkmusic@users.noreply.github.com>
2022-06-13 16:54:00 -0300

    Update ej01.py

--- Ayed/Practica Arrays/Multidimensionales/ej01.py
+++ Ayed/Practica Arrays/Multidimensionales/ej01.py
@@ -1,1 +1,17 @@
+def carga(x):
+    for i in range(f):
+        print(i)
+        for k in range(c):
+            x [i][k] = float(input('nro: '))
 
+def suma(x,y):
+    suma = 0
+    for i in range(c):
+        suma += x[y][i]
+    return suma        
+
+f, c = 30, 12
+a = f*[c*[0]]
+carga(a)
+nro = int(input('numero de fila a sumar: '))
+print(f'La suma es {suma(a,nro)} ')
