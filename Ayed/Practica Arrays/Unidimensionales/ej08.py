78ddef6a841a4739ee22a81fa4bddcd4a48b7392
Gastón <72703358+playthafunkmusic@users.noreply.github.com>
2022-06-14 13:04:21 -0300

    Update ej08.py

--- Ayed/Practica Arrays/Unidimensionales/ej08.py
+++ Ayed/Practica Arrays/Unidimensionales/ej08.py
@@ -1,10 +1,7 @@
 def muestra(x):
     print (f'Texto de {len(x)} caracteres.')
-    for i in range(len(x)):
-        if texto [i] == '.':
-            print(x[i], end='' '\n')
-        else: 
-            print(x[i], end='')            
+    for i in range(len(x)):   
+        print(x[i], end='' '\n') if texto [i] == '.' else print(x[i], end='')            
 
 texto = input('Ingresar texto: ')
 muestra(texto)
